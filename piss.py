e0fd20d2ba0b36a141a26889bc32f5654c5eab98
Witchtel <114418645+Witchtel01@users.noreply.github.com>
2024-11-01 19:35:17 -0400

    Finished.

--- piss.py
+++ piss.py
@@ -1,28 +1,35 @@
 import math
 from typing import List
 
-slurryFlowRate = (835200 * 0.00378541) / (24 * 60 * 60)
+import progressbar
+
+slurryGallonsPerDay = 835200
+slurryFlowRate = (slurryGallonsPerDay * 0.00378541) / (24 * 60 * 60)
 massFlowRate = slurryFlowRate * (1599 * 0.20 + 0.2 * 1311 + 0.60 * 977)
 
 
 class Operation:
-    def __init__(self, energyConsumption : int, efficiency : float, cost : int):
+    def __init__(self, energyConsumption : float, efficiency : float, cost : float):
         self.cons = energyConsumption
         self.efficiency = efficiency
         self.cost = cost
 
 class Pump:
-    def __init__(self, efficiency, cost):
-        self.efficiency = efficiency
-        self.cost = cost
+    def __init__(self, quality : int, perfRating : float):
+        with open("pumps.txt") as f:
+            lines = f.readlines()
+            linesplit = [_.split() for _ in lines]
+        self.efficiency = [0.8, 0.83, 0.86, 0.89, 0.92][quality]
+        self.cost = float(linesplit[int(perfRating / 3 - 2)][quality])
         
 class Pipe:
-    def __init__(self, frictionFactor, diameter, cost, length, _type = "liquid"):
-        self.frictionFactor = frictionFactor
+    def __init__(self, quality, diameter, length):
+        with open("pipes.txt") as f:
+            linesplit = [_.split() for _ in f.readlines()]
+        self.frictionFactor = [0.05, 0.03, 0.02, 0.01, 0.005, 0.002][quality]
         self.diameter = diameter
-        self.cost = cost
         self.length = length
-        self.type = _type
+        self.cost = self.length * float(linesplit[int(diameter * 100 - 10)][quality])
 
 class Bend:
     def __init__(self, lossCoefficient, diameter, cost):
@@ -31,18 +38,18 @@ class Bend:
         self.cost = cost
 
 class Valve:
-    def __init__(self, flowCoefficient, diameter, cost):
-        self.flowCoefficient = flowCoefficient
+    def __init__(self, quality, diameter):
+        with open("valves.txt") as f:
+            linesplit = [_.split() for _ in f.readlines()]
+        self.flowCoefficient = [800, 700, 600, 500][quality]
         self.diameter = diameter
-        self.cost = cost
+        self.cost = int(linesplit[int(diameter * 100 - 10)][quality])
 
 class Site:
-    def __init__(self, pipeList : List[Pipe], pumpList : List[Pump], bendList : List[Bend], valveList : List[Valve], cost: int):
+    def __init__(self, pipeList : List[Pipe], pump : Pump, valve : Valve):
         self.pipeList = pipeList
-        self.pumpList = pumpList
-        self.bendList = bendList
-        self.valveList = valveList
-        self.cost = cost
+        self.pump = pump
+        self.valve = valve
 
 
 fermenterList = [
@@ -73,13 +80,6 @@ filterList = [
     Operation(51000, 0.98, 480000)
 ]
 
-pumpList = [
-    Pump(0.8, 390000),
-    Pump(0.83, 460000),
-    Pump(0.86, 560000),
-    Pump(0.89, 670000),
-    Pump(0.92, 808000)
-]
 
 def electricalEnergyIn(fermenter : Operation, distiller : Operation, dehydration : Operation, _filter : Operation, pump : Operation) -> float:
     return sum(fermenter.cons, distiller.cons, dehydration.cons, _filter.cons, pump.cons)
@@ -103,21 +103,31 @@ def valveLoss(valve : Valve, density : float, flowRate : float):
     return density * flowRate * hdw
 
 
-def calculate(fermenter : Operation, distiller : Operation, dehydration : Operation, _filter : Operation, site : Site) -> dict:
+def calculate(
+        fermenter : Operation,
+        distiller : Operation,
+        dehydration : Operation,
+        _filter : Operation,
+        site : Site
+    ) -> dict:
+    
     totalEnergyConsumed = 0
     sugarIn = slurryFlowRate * 0.20 * 1599
     fiberIn = slurryFlowRate * 0.20 * 1311
     waterIn = slurryFlowRate * 0.60 * 997
     ethanolIn = 0
     energyIn = kineticEnergyIn(site.pipeList[0])
-    totalEnergyConsumed += pumpLoss(site.pumpList[0], energyIn)
+    totalEnergyConsumed += pumpLoss(site.pump, energyIn)
+    totalCost = 0
     
     density = (sugarIn + fiberIn + waterIn + ethanolIn) / ((sugarIn / 1599) + (fiberIn / 1311) + (waterIn / 977) + (ethanolIn / 789))
     flowRate = (sugarIn + fiberIn + waterIn + ethanolIn) / density
+    totalCost += site.pump.cost * flowRate
+    totalCost += fermenter.cost * flowRate
     
     totalEnergyConsumed += pipeFriction(site.pipeList[0], density, flowRate)
     
-    totalEnergyConsumed += valveLoss(site.valveList[0], density, flowRate)
+    totalEnergyConsumed += valveLoss(site.valve, density, flowRate)
     ethanolOut = 0.51 * sugarIn * fermenter.efficiency
     sugarOut = sugarIn * (1 - fermenter.efficiency)
     waterOut = waterIn
@@ -138,10 +148,11 @@ def calculate(fermenter : Operation, distiller : Operation, dehydration : Operat
     density = (sugarIn + fiberIn + waterIn + ethanolIn) / ((sugarIn / 1599) + (fiberIn / 1311) + (waterIn / 977) + (ethanolIn / 789))
     flowRate = (sugarIn + fiberIn + waterIn + ethanolIn) / density
 
-    totalEnergyConsumed += valveLoss(site.valveList[1], density, flowRate)
-    totalEnergyConsumed += pipeFriction(site.pipeList[1], density, flowRate)
-    totalEnergyConsumed += valveLoss(site.valveList[2], density, flowRate)
 
+    totalEnergyConsumed += valveLoss(site.valve, density, flowRate)
+    totalEnergyConsumed += pipeFriction(site.pipeList[1], density, flowRate)
+    totalEnergyConsumed += valveLoss(site.valve, density, flowRate)
+    totalCost += _filter.cost * flowRate
     
     fiberOut = fiberIn * (1 - _filter.efficiency)
     waterOut = waterIn
@@ -160,10 +171,10 @@ def calculate(fermenter : Operation, distiller : Operation, dehydration : Operat
     density = (sugarIn + fiberIn + waterIn + ethanolIn) / ((sugarIn / 1599) + (fiberIn / 1311) + (waterIn / 977) + (ethanolIn / 789))
     flowRate = (sugarIn + fiberIn + waterIn + ethanolIn) / density
 
-    totalEnergyConsumed += valveLoss(site.valveList[3], density, flowRate)
+    totalEnergyConsumed += valveLoss(site.valve, density, flowRate)
     totalEnergyConsumed += pipeFriction(site.pipeList[2], density, flowRate)
-    totalEnergyConsumed += valveLoss(site.valveList[4], density, flowRate)
-    
+    totalEnergyConsumed += valveLoss(site.valve, density, flowRate)
+    totalCost += distiller.cost * flowRate
     
     waterOut = (waterIn * ethanolIn * ((1 / distiller.efficiency) - 1)) / (waterIn + sugarIn + fiberIn)
     sugarOut = (sugarIn * ethanolIn * ((1/distiller.efficiency) - 1)) / (waterIn + sugarIn + fiberIn)
@@ -183,9 +194,10 @@ def calculate(fermenter : Operation, distiller : Operation, dehydration : Operat
     density = (sugarIn + fiberIn + waterIn + ethanolIn) / ((sugarIn / 1599) + (fiberIn / 1311) + (waterIn / 977) + (ethanolIn / 789))
     flowRate = (sugarIn + fiberIn + waterIn + ethanolIn) / density
 
-    totalEnergyConsumed += valveLoss(site.valveList[5], density, flowRate)
+    totalEnergyConsumed += valveLoss(site.valve, density, flowRate)
     totalEnergyConsumed += pipeFriction(site.pipeList[3], density, flowRate)
-    totalEnergyConsumed += valveLoss(site.valveList[6], density, flowRate)
+    totalEnergyConsumed += valveLoss(site.valve, density, flowRate)
+    totalCost += dehydration.cost * flowRate
     
     waterOut = waterIn * (1 - dehydration.efficiency)
     ethanolOut = ethanolIn
@@ -203,9 +215,10 @@ def calculate(fermenter : Operation, distiller : Operation, dehydration : Operat
     density = (sugarIn + fiberIn + waterIn + ethanolIn) / ((sugarIn / 1599) + (fiberIn / 1311) + (waterIn / 977) + (ethanolIn / 789))
     flowRate = (sugarIn + fiberIn + waterIn + ethanolIn) / density
     
-    totalEnergyConsumed += valveLoss(site.valveList[7], density, flowRate)
+    totalEnergyConsumed += valveLoss(site.valve, density, flowRate)
     totalEnergyConsumed += pipeFriction(site.pipeList[4], density, flowRate)
     
+    purity = ethanolOut / (waterOut + sugarOut + fiberOut + ethanolOut)
     
     return {
         "sugarOut" : sugarOut,
@@ -216,5 +229,47 @@ def calculate(fermenter : Operation, distiller : Operation, dehydration : Operat
         "waterWaste" : waterWaste,
         "sugarWaste" : sugarWaste,
         "CO2Waste" : co2Waste,
-        "energyConsumed" : totalEnergyConsumed
+        "energyConsumed" : totalEnergyConsumed,
+        "purity" : purity,
+        "totalCost" : totalCost
     }
+
+
+
+file = open("piss.txt", "w")
+purityFile = open("piss2.txt", "w")
+
+widgets = [' [',
+           progressbar.Timer(format='elapsed time: %(elapsed)s'),
+           '] ',
+           progressbar.Bar("█"),
+           " (",
+           progressbar.ETA(),
+           ") "
+]
+
+max = len(fermenterList) * len(distillerList) * len(dehydrationList) * len(filterList) * 6 * 6 * 5 * 4
+
+bar = progressbar.ProgressBar(max_value=max, widgets=widgets).start()
+
+
+for fermenter in fermenterList:
+    for distiller in distillerList:
+        for dehydrater in dehydrationList:
+            for filt in filterList:
+                for pipeQuality in range(6):
+                    for universalDiameter in [0.1, 0.11, 0.12, 0.13, 0.14, 0.15]:
+                        for pumpQuality in range(5):
+                            for valveQuality in range(4):
+                                site = Site(
+                                    [Pipe(pipeQuality, universalDiameter, _) for _ in [10.78, 1.53, 8.62, 1.53, 3.05]],
+                                    Pump(pumpQuality, 27),
+                                    Valve(2, universalDiameter)
+                                )
+                                calc = calculate(fermenter, distiller, dehydrater, filt, site)
+                                file.write(str(calc)+"\n\n")
+                                purityFile.write(str(calc.get("purity"))+"\n\n")
+                                bar.next()
+
+file.close()
+purityFile.close()
