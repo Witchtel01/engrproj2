1dd9e1f0dcb49931fc88fc49338b8567236de765
Witchtel <engineeringkerbals@gmail.com>
2024-11-05 20:55:36 -0500

    removed venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\jagra\Desktop\Freshman Year\Engr\project 2\.venv
+set VIRTUAL_ENV=c:\Users\jagra\Desktop\Freshman Year\Engr\proj2-2\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
